abf14f7757a97fd0f435948c43aeb4b3d54d72a5
Juliano Amaral <jaygovinda13@gmail.com>
2023-02-08 00:12:16 -0300

    Exercício um emplementado, continuar a partir do 2

---
+++ exercicio1/script.js
@@ -0,0 +1,21 @@
+const seuNome = () => {
+    const nome = prompt("Qual seu nome")
+    return console.log(`Ola, ${nome}`)
+}
+
+
+const tab = (num,) => {
+    return console.log(`A tabuade ${num} é 
+        ${num * 1},
+        ${num * 2},
+        ${num * 3},
+        ${num * 4},
+        ${num * 5},
+        ${num * 6},
+        ${num * 7},
+        ${num * 8},
+        ${num * 9},
+        ${num * 10}`)
+}
+
+tab()
